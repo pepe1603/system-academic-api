86d40c1c11aeee133a25ae44e69bf67a416c7835
pepe1603 <b180022@unach.mx>
2026-04-21 09:27:06 -0600

    fix: change registration endpoint to /api/registration

--- src/main/java/com/academic_system/controller/registration/RegistrationController.java
+++ src/main/java/com/academic_system/controller/registration/RegistrationController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/auth/registration")
+@RequestMapping("/api/registration")
 @RequiredArgsConstructor
 public class RegistrationController {
 
